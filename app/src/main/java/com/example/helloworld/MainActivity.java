a69b82020a7febb006eb11376b2b9e7f90d1abfc
Natasha Laurie <natusik711@gmail.com>
2021-11-29 20:19:45 +0300

    add 3d change

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //3d change
         // some commit
 // first
     }
