ad66efb8049b31186116504f180c96eee6074d55
User <taranovegor91@gmail.com>
2021-11-30 00:14:09 +0300

    первое приложение готово

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnApp1,btnApp2,btnApp3,btnApp4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnApp1=findViewById(R.id.button1);
+        btnApp2=findViewById(R.id.button2);
+        btnApp3=findViewById(R.id.button3);
+        btnApp4=findViewById(R.id.button4);
+        btnApp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,App_1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
